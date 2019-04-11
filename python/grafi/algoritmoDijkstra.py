517f83415314ef5002b850fabc30ef4b1002fceb
NikoWar <43783505+NikoWar@users.noreply.github.com>
2019-04-11 11:19:17 +0200

    Dijkstra finale

--- python/grafi/algoritmoDijkstra.py
+++ python/grafi/algoritmoDijkstra.py
@@ -16,26 +16,30 @@ labelList[0] = 0                    #distanza dal nodo sorgente = 0
 
 unexploredNode = [0,1,2,3,4,5,6,7]   #nodi da esplorare
 
-tempLabel = 0
+cntPasso=0
 
 while len(unexploredNode)>0:
-    print(labelList)
+    print("Passo numero: %d"% cntPasso)
+    print("Nodi inesplorati: ", unexploredNode)
+    print("Lista delle label: ", labelList)
+    print("-----------------------------------------------")
     #scelgo il nodo con label minore(k)
-    tempList = [math.inf] * len(graph)
-    for i in unexploredNode:
-        tempList[i]=labelList[i]    #valore della labelList[i] 
+    infiniteList = [math.inf] * len(graph)
     
-    minimun = min(tempList)   #trovo il minimo
+    for i in unexploredNode:
+        infiniteList[i]=labelList[i]    #valore della labelList[i] 
+    
+    minimun = min(infiniteList)   #trovo il minimo
 
     if(minimun==math.inf):  #controllo per uscire dal ciclo
         break
     
-    indexMin = tempList.index(minimun)  #prendo l'indice del valore minimo
-    tempLabel += labelList[indexMin]    #sommo per sapere il percorso fatto finora
-
-    for j in unexploredNode:         #ciclo for con ricerca dei nodi collegati a k per indici non ancora esplorati
-        if(graph[indexMin][j]!=0):   #verifico che sia collegato e che non sia se stesso
-            if(labelList[j]>tempLabel+graph[indexMin][j]):
-                labelList[j]=tempLabel+graph[indexMin][j]   #per ogni nuovo nodo sommo (valore label(k)+peso arco di quel nodo) il valore della sua label e controllo se sono minori così sostituisco sennò niente
-
-    unexploredNode.remove(indexMin)    #rimuovo l'indice visitato    
+    currentNode = infiniteList.index(minimun)  #prendo l'indice del valore minimo
+    for node, weight in enumerate(graph[currentNode]):       #ciclo for con ricerca dei nodi collegati a k per indici non ancora esplorati
+        if(weight!=0):   #verifico che sia collegato e che non sia se stesso
+            newLabel = labelList[currentNode] + weight
+            if(newLabel<labelList[node]):
+               labelList[node] = newLabel     #per ogni nuovo nodo sommo (valore label(k)+peso arco di quel nodo) il valore della sua label e controllo se sono minori così sostituisco sennò niente
+    
+    cntPasso=cntPasso+1
+    unexploredNode.remove(currentNode)    #rimuovo l'indice visitato    
