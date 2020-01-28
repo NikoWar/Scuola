e93658b36c283b709e719135d64e9b3c14772b6a
NikoWar <43783505+NikoWar@users.noreply.github.com>
2020-01-28 10:24:52 +0000

    Add files via upload

--- python/WebServices/loginpage/client.py
+++ python/WebServices/loginpage/client.py
@@ -2,18 +2,19 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-URL = "http://127.0.0.1:5000/register"
-IP_SERVER = "127.0.0.1"
-PORT = 5000
+URL = "http://127.0.0.1:5000/register"  #url standard
+IP_SERVER = "127.0.0.1"                 #ip server
+PORT = 5000                             #processo
 
 s.connect((IP_SERVER, PORT))
 strToSend=""
 
-username = "Giruz"
-password = "1234"
+username = "Niko"                      #credenziali per loggare
+password = "ciao"
 
 method = "GET" #/POST
 
+#formulazione richiesta
 request_type = f"{method} {URL} HTTP/1.0"
 body_entity = f"username={username}&password={password}"
 headers = f'Content-Type: application/x-www-form-urlencoded\nContent-Length: {len(body_entity)}'
@@ -30,11 +31,11 @@ while data !='':
     code.append(data)
     print("Server: %s" % data)
 
-html_code = code[-2]
-html_code = html_code[html_code.find("<html>"):]
+html_code = code[-2]    #prende la penultima istanza del vettore contenente stringhe e codice
+html_code = html_code[html_code.find("<html>"):]    #carico tutte le informazioni a partire da "<html>"
 
 out_file = open('register.html', 'w')
-out_file.write(html_code)
+out_file.write(html_code)   #scrivo su file il codice html
 out_file.close()
 
 s.close()
