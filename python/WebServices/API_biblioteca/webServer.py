62ffa5b37e985c5d3c7c4267decf8624a75b6b29
NikoWar <43783505+NikoWar@users.noreply.github.com>
2020-04-21 11:32:59 +0200

    Update webServer.py

--- python/WebServices/API_biblioteca/webServer.py
+++ python/WebServices/API_biblioteca/webServer.py
@@ -51,39 +51,3 @@ if __name__== "__main__":   #chiamata al main "riciclabile"
     sqliteConnection = sqlite3.connect('dbBooks.db')
     cursor = sqliteConnection.cursor()
     app.run()
-
-
-    '''
-        HTTP/1.0 200 OK
-        Content-Type: application/json
-        Content-Length: 245
-        Server: Werkzeug/0.16.1 Python/3.8.1
-        Date: Tue, 03 Mar 2020 10:26:53 GMT
-
-        Host: 127.0.0.1:5000
-        User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0
-        Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-        Accept-Language: en-US,en;q=0.5
-        Accept-Encoding: gzip, deflate
-        Connection: keep-alive
-        Upgrade-Insecure-Requests: 1
-        Cache-Control: max-age=0
-
-        QUERY ESEMPIO
-        HTTP/1.0 200 OK
-        Content-Type: application/json
-        Content-Length: 87
-        Server: Werkzeug/0.16.1 Python/3.8.1
-        Date: Tue, 03 Mar 2020 10:28:10 GMT
-
-        Host: 127.0.0.1:5000
-        User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0
-        Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-        Accept-Language: en-US,en;q=0.5
-        Accept-Encoding: gzip, deflate
-        Connection: keep-alive
-        Upgrade-Insecure-Requests: 1
-        Cache-Control: max-age=0
-
-        QUERY MODIFICATA
-    '''
