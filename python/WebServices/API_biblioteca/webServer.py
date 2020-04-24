bf23dc93ab322bc7514feb59e99d9ee03d601d35
NikoWar <43783505+NikoWar@users.noreply.github.com>
2020-04-24 15:38:36 +0000

    Add files via upload

--- python/WebServices/API_biblioteca/webServer.py
+++ python/WebServices/API_biblioteca/webServer.py
@@ -9,45 +9,101 @@ def home():
     return "<h1>Biblioteca online</h1><p>Prototipo di web API.</p>"
 
 
-@app.route('/api/v1/resources/books/all', methods=['GET'])
+@app.route('/allBooks', methods=['GET'])
 def api_all():
-    return jsonify(cursor.execute(f"""
-    SELECT * 
-    FROM Books;
-    """))
+    try:
+        sqliteConn = sqlite3.connect('dbBooks.db')
+        cursor = sqliteConn.cursor()
 
-@app.route('/api/v1/resources/books', methods=['GET'])
+        cursor.execute("SELECT * FROM Books")
+        allBooks = cursor.fetchall()
+
+    except sqlite3.Error as error:
+        print("Error: " + error)
+
+    finally:
+        if (sqliteConn):
+            print('chiusura connessione DB')
+            sqliteConn.close()
+    
+    return jsonify(allBooks)
+
+@app.route('/booksById', methods=['GET'])
 def api_id():
     if 'id' in request.args:
         id = int(request.args['id'])
     else:
         return "Error: No id field provided. Please specify an id."
     
-    results = []
-    results.append(cursor.execute(f"""
-    SELECT id 
-    FROM Books
-    WHERE id == {id};
-    """))
+    try:
+        sqliteConn = sqlite3.connect("dbBooks.db")
+        cursor = sqliteConn.cursor()
+
+        cursor.execute(f"""
+        SELECT title, author, published
+        FROM Books
+        WHERE id == {id};
+        """)
+
+        results = cursor.fetchall()
+    except sqlite3.Error as error:
+        print('Error: ' + error)
+    
+    finally:
+        if (sqliteConn):
+            print('chiusura connessione DB')
+            sqliteConn.close()
+    
     return jsonify(results)
 
-@app.route('/api/v1/resources/books', methods=['GET'])
+@app.route('/booksByTitle', methods=['GET'])
 def api_title():
     if 'title' in request.args:
-        title = request.args
+        title = request.args['title']
     else:
         return "Error: No title field provided. Please specify a title."
     
-    results = []
+    try:
+        sqliteConn = sqlite3.connect("dbBooks.db")
+        cursor = sqliteConn.cursor()
 
-    results.append(cursor.execute(f"""
-    SELECT title 
-    FROM Books
-    WHERE title == {title};
-    """))
+        cursor.execute(f"""
+        SELECT title, author, published
+        FROM Books
+        WHERE title == {title};
+        """)
+
+        results = cursor.fetchall()
+    except sqlite3.Error as error:
+        print('Error: ' + error)
+    
+    finally:
+        if (sqliteConn):
+            sqliteConn.close()
+    
     return jsonify(results)
 
+@app.route('/insertBook', methods=['GET'])
+def insertBook():
+    if 'title' in request.args and 'author' in request.args and 'published' in request.args:
+        title = request.args['title']
+        author = request.args['author']
+        published = int(request.args['published'])
+    else:
+        return 'Error: possible missing arguments'
+    
+    try:
+        sqliteConn = sqlite3.connect("dbBooks.db")
+        cursor = sqliteConn.cursor()
+
+        cursor.execute(f'INSERT INTO Books(title, author, published) VALUES ("{title}", "{author}", "{published}");')
+        sqliteConn.commit()
+    except sqlite3.Error as error:
+        print('Error: ' + error)
+    finally:
+        sqliteConn.close()
+
+    return 'Libro caricato'
+
 if __name__== "__main__":   #chiamata al main "riciclabile" 
-    sqliteConnection = sqlite3.connect('dbBooks.db')
-    cursor = sqliteConnection.cursor()
     app.run()
