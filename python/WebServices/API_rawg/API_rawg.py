fec5cfb59e65e8faf0bf042f4b8bcbfa7668314e
NikoWar <43783505+NikoWar@users.noreply.github.com>
2020-05-02 09:27:19 +0200

    Add files via upload

--- python/WebServices/API_rawg/API_rawg.py
+++ python/WebServices/API_rawg/API_rawg.py
@@ -1,46 +1,77 @@
 import flask
-from flask import request, jsonify
+import requests
 import sqlite3
 
-app = flask.Flask(__name__)
+URL = 'https://api.rawg.io/api/games?dates=2001-01-01,2001-12-31&ordering=-rating'
 
-@app.route('/insertRecord', methods=['GET'])
-def insertBook():
-    if 'title' in request.args and 'author' in request.args and 'published' in request.args:
-        title = request.args['title']
-        author = request.args['author']
-        published = int(request.args['published'])
-    else:
-        return 'Error: possible missing arguments'
-    
+def getData(URL):
+    raw_data = requests.get(url=URL)
+    print(raw_data)
+
+    data = raw_data.json()
+    insertVideoGames(data)
+
+def insertVideoGames(data):
     try:
-        sqliteConn = sqlite3.connect("db_videogames.db")
-        cursor = sqliteConn.cursor()
+        sqlConn = sqlite3.connect('db_videogames.db')
+        cursor = sqlConn.cursor()
+
+        for d in data['results']:
+            cursor.execute(f'''INSERT INTO Videogames 
+                            (slug, name, playtime, released, background_image, rating, rating_top, ratings_count,
+                            reviews_text_count, added, suggestions_count, id_videogame, score, reviews_count, saturated_color, dominant_color
+                        ) VALUES 
+                            ("{d['slug']}", 
+                            "{d['name']}", 
+                            "{d['playtime']}", 
+                            "{d['released']}", 
+                            "{d['background_image']}", 
+                            "{d['rating']}",
+                            "{d['rating_top']}", 
+                            "{d['ratings_count']}", 
+                            "{d['reviews_text_count']}", 
+                            "{d['added']}", 
+                            "{d['suggestions_count']}", 
+                            "{str(d['id'])}", 
+                            "{d['score']}", 
+                            "{d['reviews_count']}", 
+                            "{d['saturated_color']}", 
+                            "{d['dominant_color']}");''')
+            sqlConn.commit()
+
+    except sqlite3.Error as error:
+        print('Error: ' + str(error))
+    finally:
+        sqlConn.close()
+
+    return 'Libro caricato'
+getData(URL)
+'''
+def insertBook():
 
         cursor.execute(f'INSERT INTO Videogames(slug, name, playtime, released, background_image, rating, rating_top, ratings_count,
                         reviews_text_count, added, suggestions_count, id_videogame, score, reviews_count, saturated_color, dominant_color
                         ) VALUES ("{title}", "{author}", "{published}");')
         sqliteConn.commit()
    
-   ''' 
-    "id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-	"slug"	TEXT,
-	"name"	TEXT,
-	"playtime"	INTEGER DEFAULT 0,
-	"released"	DATE,
-	"background_image"	TEXT,
-	"rating"	DECIMAL(1 , 2),
-	"rating_top"	DECIMAL(1 , 2),
-	"ratings_count"	INTEGER,
-	"reviews_text_count"	INTEGER,
-	"added"	INTEGER,
-	"suggestions_count"	INTEGER,
-	"id_videogame"	INTEGER,
-	"score"	DECIMAL(1 , 2),
-	"reviews_count"	INTEGER,
-	"saturated_color"	TEXT,
+
+    "id"	
+	"slug"	
+	"name"	
+	"playtime"	
+	"released"	
+	"background_image"	
+	"rating"	
+	"rating_top"	
+	"ratings_count"	
+	"reviews_text_count"	
+	"added"	
+	"suggestions_count"	
+	"id_videogame"	
+	"score"	
+	"reviews_count"	
+	"saturated_color"	
 	"dominant_color"
-    '''
 
     except sqlite3.Error as error:
         print('Error: ' + error)
@@ -51,3 +82,4 @@ def insertBook():
 
 if __name__== "__main__":   #chiamata al main "riciclabile" 
     app.run()
+'''
